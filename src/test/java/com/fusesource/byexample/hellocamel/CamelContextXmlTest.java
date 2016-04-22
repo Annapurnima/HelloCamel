abfb7770a87d76fd062b377b2e5b6b2c2953ca96
Annapurna <bapi2k14@gmail.com>
2016-04-22 17:58:12 +0530

    changed to HeLLo

--- src/test/java/com/fusesource/byexample/hellocamel/CamelContextXmlTest.java
+++ src/test/java/com/fusesource/byexample/hellocamel/CamelContextXmlTest.java
@@ -33,7 +33,7 @@ public class CamelContextXmlTest extends CamelSpringTestSupport {
     public void testCamelRoute() throws Exception {
         String response = inputEndpoint.requestBody((Object) "Camel", String.class);
 
-        assertEquals("HElLo Camel", response);
+        assertEquals("HeLLo Camel", response);
     }
 
     @Override
